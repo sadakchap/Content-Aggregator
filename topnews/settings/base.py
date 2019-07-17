e8b9bbda96e1f89dd5c1d2c961864ac8c3233f6c
prerna <aliceprerna@gmail.com>
2019-07-17 18:11:41 +0530

    back to development

--- topnews/settings/base.py
+++ topnews/settings/base.py
@@ -1,5 +1,4 @@
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -9,11 +8,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'cf#c1v^*g@j6ii_&+*%9)_vs=&@#1epugi2ds!we4$y&jemq2h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-
 
 # Application definition
 
@@ -102,10 +99,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+VENV_PATH = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
-
-django_heroku.settings(locals())
--- topnews/settings/prod.py
+++ topnews/settings/prod.py
@@ -1,10 +1,12 @@
 from .base import *
-import dj_database_url
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['latestnewstrends.herokuapp.com']
 
-DATABASES = {}
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
