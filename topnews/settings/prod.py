1114d2506428393240025c73309ebd2f8a46fd13
prerna <aliceprerna@gmail.com>
2019-07-17 17:59:19 +0530

    changed database in prod

--- topnews/settings/prod.py
+++ topnews/settings/prod.py
@@ -1,12 +1,10 @@
 from .base import *
+import dj_database_url
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['latestnewstrends.herokuapp.com']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
