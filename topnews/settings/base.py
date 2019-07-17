c2d18057c3d75e08e8f83fe93415f8f42290fd98
prerna <aliceprerna@gmail.com>
2019-07-17 17:39:54 +0530

    changes static_root

--- topnews/settings/base.py
+++ topnews/settings/base.py
@@ -104,8 +104,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
-VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
