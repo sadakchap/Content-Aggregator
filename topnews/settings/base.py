4338014d0a871ee230a4a8d21dbffcf7653d1271
prerna <aliceprerna@gmail.com>
2019-07-17 17:31:33 +0530

    added django-heroku

--- topnews/settings/base.py
+++ topnews/settings/base.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -107,3 +108,5 @@ VENV_PATH = os.path.dirname(BASE_DIR)
 STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
+django_heroku.settings(locals())
